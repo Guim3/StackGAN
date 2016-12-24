b7c8a50292430a5576722753008384958380a8ff
Guim <guimperarnau@hotmail.com>
2016-12-24 18:25:56 +0100

    Load images and labels as numpy arrays, not lists

--- data/DatasetReader.py
+++ data/DatasetReader.py
@@ -52,28 +52,37 @@ class DatasetReader:
         data_path = os.path.join(self.__path + "/images_and_texts/")
         assert data_path, "Didn't find 'images_and_texts' folder in %s" % self.__path
 
-        # Output variables
-        images = []
-        texts = []
-        labels = []
-
         # List all files
         folder_list = os.listdir(data_path)
         folder_list.sort()
         folder_iterator = filter(lambda element: os.path.isdir(data_path + element), folder_list)
 
-        label_idx = 0
-        for folder in folder_iterator: # Every folder contains images from the same label / class
+        # Output variables
+        images = np.empty([0,0,0,0], dtype=int) # This array shape will be set once the first image is read
+        labels = []                             # This list will be converted to a 1-dimensional array
+        texts = []                              # This is a list, as it will store strings of different lengths
+        
+        first_iteration = True  # We need to read an image first to get its size
+
+        for folder_idx, folder in enumerate(folder_iterator): # Every folder contains images from the same label / class
 
             # List all images and text files within the folder
             file_list = os.listdir(data_path + folder)
             file_list.sort() # Order is important because image and text files need to match
 
             # Filter images and texts using their extension
-            im_iterator = filter(lambda x: x.endswith(('.jpg')), file_list)
+            im_list = list(filter(lambda x: x.endswith(('.jpg')), file_list))
             txt_iterator = filter(lambda x: x.endswith(('.txt')), file_list)
+            
+            # Get image size to initialize images array
+            if first_iteration == True:
+                im_sz = misc.imread( os.path.join( os.path.join(data_path, folder), im_list[0] ) ).shape
 
-            for im_file, txt_file in zip(im_iterator, txt_iterator):
+            # Initialize temporal array that will be concatenated to the main 'images' array
+            tmp_images = np.zeros([len(im_list), im_sz[0], im_sz[1], im_sz[2]], dtype=int)
+
+            i = 0 # Index for 'tmp_images' array
+            for im_file, txt_file in zip(im_list, txt_iterator):
 
                 # Sanity check: make sure image and text file match
                 tmp1 = im_file
@@ -86,7 +95,16 @@ class DatasetReader:
                 element_path = os.path.join(data_path, folder)
 
                 # Read image
-                images.append( misc.imread(os.path.join(element_path, im_file)))
+                im = misc.imread(os.path.join(element_path, im_file))
+
+                # Sanity check: make sure all images have same size
+                assert im_sz == im.shape, ("Images from %s dataset have different sizes: " + \
+                                           str(im_sz) +  ", " + str(im.shape) +  ". Have you preprocessed" + \
+                                          " the dataset so all images have equal size?\n" + \
+                                           "Conflicting image: %s") % (self.__dataset, os.path.join(element_path, im_file))
+
+                tmp_images[i,] = im
+
 
                 # Read texts
                 with open(os.path.join(element_path, txt_file), 'r') as f:
@@ -94,14 +112,24 @@ class DatasetReader:
                 texts.append(lines)
 
                 # Set label
-                labels.append(label_idx)
+                labels.append(folder_idx)
 
-            print("\t\t%d/%d" % (label_idx, len(folder_list)), end='\r')
+                i += 1
 
+            if first_iteration:
+                images = tmp_images
+                first_iteration = False
+            else:
+                # Concatenate tmp_images to images (1st dimension concat)
+                images = np.vstack((images, tmp_images))
 
-            label_idx += 1
+            print("\t\t%d/%d" % (folder_idx, len(folder_list)), end='\r')
+
 
         print("Done!")
+
+        labels = np.asarray(labels)
+
         return images, texts, labels
 
 
